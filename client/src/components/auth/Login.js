5f2a65f037a510ad81af26bebd81bec8e4f314e5
Grmonta <grmonta@gmail.com>
2019-06-16 10:23:27 -0500

    onsubmit events added

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,62 @@
-import React from 'react';
-
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 const Login = () => {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  //by changing the key to be in brackets you can know use it on
+  // every input below
+
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async e => {
+    e.preventDefault();
+
+    console.log('Success');
+  };
+
+  return (
+    <Fragment>
+      <section className='container'>
+        <h1 className='large text-primary'>Sign In</h1>
+        <p className='lead'>
+          <i className='fas fa-user' /> Sign Into Your Account
+        </p>
+        <form className='form' onSubmit={e => onSubmit(e)}>
+          <div className='form-group' />
+          <div className='form-group'>
+            <input
+              type='email'
+              placeholder='Email Address'
+              name='email'
+              value={email}
+              onChange={e => onChange(e)}
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              placeholder='Password'
+              name='password'
+              value={password}
+              onChange={e => onChange(e)}
+              minLength='6'
+            />
+          </div>
+          <div className='form-group' />
+          <input type='submit' className='btn btn-primary' value='Login' />
+        </form>
+        <p className='my-1'>
+          Don't have an account? <Link to='/register'>Sign up</Link>
+        </p>
+      </section>
+    </Fragment>
+  );
 };
 
 export default Login;
