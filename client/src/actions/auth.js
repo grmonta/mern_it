1faaf5b6d686e2a03f03a413fd71be09b5209fac
Grmonta <grmonta@gmail.com>
2019-06-23 14:48:02 -0500

    auth

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,23 +1,28 @@
 import axios from 'axios';
+import { setAlert } from './alert';
 import { REGISTER_SUCCESS, REGISTER_FAIL } from './types';
 
 //REGISTER USER
-export const register = ({ name, email, password}) => async dispatch => {
-  const config - {
+export const register = ({ name, email, password }) => async dispatch => {
+  const config = {
     headers: {
       'Content-Type': 'application/json'
     }
-  }
-  const body = JSON.stringfy({ name, email, password});
+  };
+  const body = JSON.stringfy({ name, email, password });
   try {
-    const res = await.axios.post('api/users', body, config);
+    const res = await axios.post('api/users', body, config);
     dispatch({
       type: REGISTER_SUCCESS,
       payload: res.data
-    })
-  } catch(err){
+    });
+  } catch (err) {
+    const errors = err.response.data.errors;
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
     dispatch({
       type: REGISTER_FAIL
     });
   }
-}
+};
